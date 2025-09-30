79190aa1af3df899dffc27e13125abc9f71f9a74
clearcutleads-create <clearcutleads@gmail.com>
2025-09-30 10:13:55 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ["*.preview.same-app.com"],
+  images: {
+    unoptimized: true
+  },
+};
+
+module.exports = nextConfig;
